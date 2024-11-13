be046e873cc74e8aad56fcd517f554c364a7aefc
enzo <enzovcnt@gmail.com>
2024-11-13 18:21:45 +0100

    console.log testing

--- js/main.js
+++ js/main.js
@@ -4,3 +4,8 @@ console.log('%c i am great text', 'font-size:50px; background:blue;');
 console.warn('nonon')
 console.error('arrête')
 console.info('le savize vous')
+
+
+const p = document.querySelector('p');
+
+console.assert(p.classList.contains('bonjour'),'cest faux');
