da800c52f6b61b1e5df1846cc6d261128a844a5c
enzo <enzovcnt@gmail.com>
2024-11-13 18:48:58 +0100

    console.log grouping

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
+const dogs = [{ name: 'Snickers', age: 2 }, { name: 'hugo', age:8 }];
+
+function makegreen() {
+    const p = document.querySelector('p');
+    p.style.color = 'green';
+    p.style.fontSize = '60px';
+}
+
+
+
+
 console.log("Hello World!");
 console.log('Welcome to the Vue.js %s application!', 'tout');
 console.log('%c i am great text', 'font-size:50px; background:blue;');
@@ -14,3 +25,12 @@ console.clear();
 
 console.log(p);
 console.dir(p);
+
+console.clear();
+
+dogs.forEach(dog => {
+    console.groupCollapsed(dog.name)
+    console.log(dog.name);
+    console.log(dog.age);
+    console.groupEnd(dog.name)
+})
