cc870573a5eb143b5eb612a0240b8f912295c9ba
enzo <enzovcnt@gmail.com>
2024-11-13 18:15:15 +0100

    console.log warn and error

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
 console.log("Hello World!");
 console.log('Welcome to the Vue.js %s application!', 'tout');
 console.log('%c i am great text', 'font-size:50px; background:blue;');
+console.warn('nonon')
+console.error('arrête')
