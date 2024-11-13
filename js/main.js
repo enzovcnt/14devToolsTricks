34c0664b1ae17b9b2df69be184ccb0dada8b0c2f
enzo <enzovcnt@gmail.com>
2024-11-13 18:16:00 +0100

    console.log info

--- js/main.js
+++ js/main.js
@@ -3,3 +3,4 @@ console.log('Welcome to the Vue.js %s application!', 'tout');
 console.log('%c i am great text', 'font-size:50px; background:blue;');
 console.warn('nonon')
 console.error('arrête')
+console.info('le savize vous')
